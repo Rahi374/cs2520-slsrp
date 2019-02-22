27ef036d56d1d4bcd6712931f9b9bd41e3702556
NathanAckerman <nga5@pitt.edu>
2019-02-21 22:45:57 -0500

    figured out the errors

--- src/router.c
+++ src/router.c
@@ -9,6 +9,7 @@
 #include <ifaddrs.h>
 #include <netdb.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #include "router.h"
 
@@ -21,8 +22,8 @@ void* writer_func2();
 int main(int argc, char *argv[])
 {
     //set up listen socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0){
+    int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(listen_sock < 0){
 	perror("Error making socket for listener");
     }else{
 	printf("Initial socket for listener successful\n");
@@ -35,11 +36,11 @@ int main(int argc, char *argv[])
     //sag.sin_addr.s_addr = htonl(inet_addr("127.0.1.1"));
     sag.sin_addr.s_addr = inet_addr("127.0.1.1");
     sag.sin_family = AF_INET;
-    bind(sock, (struct sockaddr *)&sag,sizeof(sag));
+    bind(listen_sock, (struct sockaddr *)&sag,sizeof(sag));
 
     printf("creating listener\n");
     pthread_t listener_thread;
-    pthread_create(&listener_thread, NULL, listener_func, &sock); 
+    pthread_create(&listener_thread, NULL, listener_func, &listen_sock); 
 
     usleep(3000000);
 
@@ -60,8 +61,8 @@ int main(int argc, char *argv[])
 
 
 //the listener thread
-void* listener_func(int *sock){
-    int listen_ret = listen(*sock, 100);
+void* listener_func(int *listen_sock){
+    int listen_ret = listen(*listen_sock, 100);
     if(listen_ret < 0){
 	perror("Error in listener listen call");
     }else{
@@ -70,13 +71,15 @@ void* listener_func(int *sock){
     while(1){
 	struct sockaddr_in sa2;
 	socklen_t sa2_size = sizeof(sa2);
-	int sock2 = accept(*sock, (struct sockaddr *)&sa2, &sa2_size);
-	if(sock2 < 0){
+	int accept_sock = accept(*listen_sock, (struct sockaddr *)&sa2, &sa2_size);
+	int *socket_fd = (int*)malloc(sizeof(int));
+	*socket_fd = accept_sock;
+	if(accept_sock < 0){
 	    perror("Error in accept in listener");
 	}else{
 	    printf("connection made in listener\n"); 
 	    pthread_t listener_loop_thread;
-	    pthread_create(&listener_loop_thread, NULL, listener_loop, sock2); 
+	    pthread_create(&listener_loop_thread, NULL, listener_loop, socket_fd); 
 
 	}
     }
@@ -86,13 +89,13 @@ void* listener_func(int *sock){
 void* listener_loop(int *sock){
     while(1){
 	char buf[80];
-	int n = read(sock, buf, 6);//TODO read HEADER_SIZE
+	int n = read(*sock, buf, 6);//TODO read HEADER_SIZE
 	if(n < 0){
 	    printf("error in read\n");
 	}
 	printf("Received: %s\n", buf);
 	fflush(stdout);
-	usleep(1000000);
+	//usleep(1000000);
     }
 }
 
@@ -122,7 +125,7 @@ void* writer_func(){
 	if(n < 0){
 	    perror("error in write 1\n");
 	}
-	usleep(2000000);
+	usleep(1000000);
     }
 }
 
@@ -150,6 +153,6 @@ void* writer_func2(){
 	if(n < 0){
 	    perror("error in write 2\n");
 	}
-	usleep(4000000);
+	usleep(2000000);
     }
 }
