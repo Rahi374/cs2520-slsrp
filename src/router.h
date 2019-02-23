006feec59e9c2271dbf94a4cfc6cd95ac86232fd
Paul Elder <paul.elder@ideasonboard.com>
2019-02-23 16:01:24 -0500

    router: define packet header and packet types

--- src/router.h
+++ src/router.h
@@ -1,22 +1,28 @@
 #ifndef _ROUTER_DEFS_
 #define _ROUTER_DEFS_
 
-#define PACKET_HEADER_SIZE 24 //idk what this will end up being
+// port that every router listens on
+#define LISTEN_PORT 50500
 
-#define LISTEN_PORT 50500 //port that every router listens on
-
-//packet types
-#define UI_CONTROL 0
-#define NEIGHBOR_REQUEST 1
-#define NEIGHBOR_REQUEST_RESPONSE 2
-
-
-struct packet_header {
-    int source_id;
-    int destination_id;
-    int packet_type;
-    int data;
+enum packet_type {
+	NAK,
+	ACK,
+	UI_CONTROL,
+	NEIGHBOR_REQ,
+	NEIGHBOR_REQ_RESP,
+	LSA,
+	ALIVE,
+	LINK_COST,
+	LINK_DOWN,
 };
 
+struct packet_header {
+    unsigned int source_id;
+    unsigned int destination_id;
+    enum packet_type packet_type;
+    int length;
+    unsigned int checksum_header;
+    unsigned int checksum_data;
+};
 
 #endif
