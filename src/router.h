e7a3c200b98f0c131c59420e73ac7269e2f1ead9
Paul Elder <paul.elder@ideasonboard.com>
2019-03-19 15:26:45 -0400

    lsa: add struct definitions

--- src/router.h
+++ src/router.h
@@ -72,4 +72,31 @@ struct alive_control_struct {
 	int n_port;
 };
 
+struct lsa_entry {
+	struct full_addr neighbour_id;
+	long link_cost;
+};
+
+struct lsa {
+	struct full_addr router_id;
+	struct timespec seq;
+	int age;
+	struct lsa_entry *lsa_entry_list;
+	int nentries;
+};
+
+struct lsa_sending_entry {
+	struct full_addr addr;
+	char s;
+	char a;
+};
+
+struct lsa_control_struct {
+	pid_t pid_of_control_thread;
+	struct full_addr neighbour_addr;
+	struct lsa *lsa;
+	struct lsa_sending_entry *lsa_sending_list;
+	int nentries;
+};
+
 #endif // _ROUTER_DEFS_
