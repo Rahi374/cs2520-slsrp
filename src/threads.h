368a06e70807f0b28a6d8050b2518474321df91e
Paul Elder <paul.elder@ideasonboard.com>
2019-03-20 18:30:38 -0400

    threads: lsa: implement sending thread, and skeletal generating thread

--- src/threads.h
+++ src/threads.h
@@ -5,5 +5,6 @@ void *lc_thread(void *id);
 void *alive_thread(void *id);
 void *add_neighbour_thread(void *id);
 void *lsa_sending_thread(void *id);
+void *lsa_generating_thread(void *id);
 
 #endif // _THREADS_H_
